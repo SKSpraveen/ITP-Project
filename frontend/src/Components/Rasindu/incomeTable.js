53e2b33b07cfd904b87f3c9ad1c9296a1c4102fa
Rasindu <128355483+IT22092410@users.noreply.github.com>
2024-05-06 09:40:33 +0530

    modifie income

--- frontend/src/Components/Rasindu/incomeTable.js
+++ frontend/src/Components/Rasindu/incomeTable.js
@@ -2,6 +2,8 @@ import React from "react";
 import '../../Components/Rasindu/css/table.css';
 
 function incomeTabale(){
+
+    
     return(
         <div>
             <div className="rtable-possition">
--- frontend/src/Pages/Rasindu/Income.js
+++ frontend/src/Pages/Rasindu/Income.js
@@ -9,7 +9,7 @@ function income(){
            <Header />
            <br />
            <br />
-           <IncomeTabale/>
+           <IncomeTabale />
 
         </div>
     )
